bed6bd648dd9cd92d1f99ebc3d37e75f5e95d1dc
igorokiscool <igorokiscool@gmail.com>
2019-11-19 09:34:01 +0300

    fix bug with multi query

--- index.js
+++ index.js
@@ -5,25 +5,21 @@ const port = 8580;
 
 function template(name, pass) {
     return `
-                <form action="./login">
-                    <label for="name">Name</label>
-                    <input id="name" name="name" value="${name}"/>
-                    <label for="pass">Password</label>
-                    <input id="pass" name="pass" value="${pass}" type="password"/>
-                    <input type="submit" value="Send"/>
-                </form>`;
+<form action="./">
+    <label for="name">Name</label>
+    <input id="name" name="name" value="${name}"/>
+    <label for="pass">Password</label>
+    <input id="pass" name="pass" value="${pass}" type="password"/>
+    <input type="submit" value="Send"/>
+</form>`;
 }
-let name = '';
-let pass = '';
+
 app.get('/', (request, response) => {
+    const name = request.query.name || '';
+    const pass = request.query.pass || '';
+    if (name && pass) {
+        response.send(`<h1>Hi ${name}</h1>`);
+    }
     response.send(template(name, pass));
 });
-app.get('/login', (req, res) => {
-    name = req.query.name;
-    pass = req.query.pass;
-    if (name && pass) {
-        res.status(200).send(`Hi ${name}`);
-    }
-    res.redirect(301, './');
-})
 app.listen(port);
