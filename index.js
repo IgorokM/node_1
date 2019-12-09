bc0b7998eecb1745a6406b465dfda2be48925594
Igor <mamonenko@aliasworlds.com>
2019-12-09 18:26:50 +0300

    added post request method and delete double query

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-const http = require('http');
-const url = require('url');
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
 const port = 8580;
 
 
 
 /**
  * @param {string | string[]} name
- * @param {string | string[]} pass
+ * @param {string | string[]} age
  */
-function template(name, pass) {
+function template(name, age) {
     return `
-<form action="./">
-    <label for="name">Name</label>
+<form action="./" method="POST">
+    <label for="name">Name: </label>
     <input id="name" name="name" value="${name}"/>
-    <label for="pass">Password</label>
-    <input id="pass" name="pass" value="${pass}" type="password"/>
+    <label for="age">Age: </label>
+    <input id="age" name="age" value="${age}" type="text"/>
     <input type="submit" value="Send"/>
 </form>`;
 }
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-/**
- * @param {string} urlString
- */
-function getDataUrl(urlString) {
-    try {
-        return url.parse(urlString, true).query;
-    } catch (e) {
-        console.error(e.message)
-    }
-}
+server.post('/', (req, res) => {
+    const name = req.body.name || '';
+    const age = req.body.age || '';
+    const url = require('url');
 
-function handler(req, res) {
-    const data = getDataUrl(req.url);
-    const name = data.name || '';
-    const pass = data.pass || '';
+    res.redirect(url.format({
+        pathname: '/',
+        query: {
+            name,
+            age
+        }
+    }));
+});
+
+server.get('/', (req, res) => {
+    const name = req.query.name || '';
+    const age = req.query.age || '';
     const SUCCESS = `<h1>Hi ${name}</h1>`;
-    const TEMPLATE = template(name, pass);
-    if (name && pass) {
-        res.writeHeader(200, {
-            'Content-type': 'text/html',
-            'Content-Length': Buffer.byteLength(SUCCESS)
-        }).end(SUCCESS);
-    } else if (!name && !pass) {
-        res.writeHeader(200, {
-            'Content-type': 'text/html',
-            'Content-Length': Buffer.byteLength(TEMPLATE)
-        }).end(TEMPLATE);
+    const TEMPLATE = template(name, age);
+    if (name && age) {
+        res.send(SUCCESS);
+    } else if (!name && !age) {
+        res.send(TEMPLATE);
     } else {
-        res.writeHeader(401, {
-            'Content-type': 'text/html',
-            'Content-Length': Buffer.byteLength(TEMPLATE)
-        }).end(TEMPLATE);
+        res.status(401).send(TEMPLATE);
     }
-}
-http.createServer(handler).listen(port);
+});
+
+server.listen(port);
